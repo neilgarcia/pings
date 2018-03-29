5feafdeed3098eb6259543dab75187aaafe20c6b
Neil Garcia <neil.peter.garcia@gmail.com>
2018-03-29 15:38:38 +0800

    refactor?

--- public/index.php
+++ public/index.php
@@ -6,25 +6,18 @@ require __DIR__ . '/../bootstrap/app.php';
 date_default_timezone_set('UTC');
 
 $app->post('/clear_data', function (Silex\Application $app) {
-  $stmt = $app['db']->prepare("SELECT Concat('TRUNCATE TABLE ',table_schema,'.',TABLE_NAME, ';') FROM INFORMATION_SCHEMA.TABLES where table_schema in ('pings')");
+  $stmt = $app['db']->prepare("TRUNCATE table pings");
   $stmt->execute();
-  foreach ($stmt as $row) {
-    $query = $app['db']->prepare(array_pop($row));
-    $query->execute();
-  }
   return new Response(null, 200);
 });
 
 $app->get('/devices', function (Silex\Application $app) {
-  $stmt = $app['db']->prepare("SELECT * FROM devices");
-  $stmt->execute();
+  $stmt = $app['db']->query("SELECT DISTINCT device_id FROM pings");
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
   return $app->json($result);
 });
 
 $app->post('/{deviceId}/{epochTime}', function($deviceId, $epochTime, Silex\Application $app) {
-  $stmt = $app['db']->prepare("INSERT IGNORE INTO devices (device_id) VALUES ('$deviceId')");
-  $stmt->execute();
   $stmt = $app['db']->prepare("INSERT INTO pings (device_id, epoch_time) VALUES ('$deviceId', '$epochTime')");
   $stmt->execute();
 });
@@ -55,8 +48,7 @@ $app->get('/all/{date}', function($date, Silex\Application $app) {
 $app->get('/{deviceId}/{date}', function($deviceId, $date, Silex\Application $app) {
   $startDate = strtotime($date);
   $endDate = strtotime("tomorrow", $startDate);
-  $stmt = $app['db']->prepare("SELECT * FROM pings where (pings.epoch_time >= $startDate AND pings.epoch_time < $endDate) AND device_id = '$deviceId'");
-  $stmt->execute();
+  $stmt = $app['db']->query("SELECT * FROM pings where (pings.epoch_time >= $startDate AND pings.epoch_time < $endDate) AND device_id = '$deviceId'");
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
   return $app->json($result);
 });
@@ -64,8 +56,7 @@ $app->get('/{deviceId}/{date}', function($deviceId, $date, Silex\Application $ap
 $app->get('/{deviceId}/{from}/{to}', function($deviceId, $from, $to, Silex\Application $app) {
   $startDate = is_numeric($from) ? $from : strtotime($from);
   $endDate = is_numeric($to) ? $to : strtotime($to);
-  $stmt = $app['db']->prepare("SELECT * FROM pings where (pings.epoch_time >= $startDate AND pings.epoch_time < $endDate) AND pings.device_id = '$deviceId'");
-  $stmt->execute();
+  $stmt = $app['db']->query("SELECT * FROM pings where (pings.epoch_time >= $startDate AND pings.epoch_time < $endDate) AND pings.device_id = '$deviceId'");
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
   return $app->json($result);
 });
